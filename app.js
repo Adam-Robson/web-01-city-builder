6bfc93464fb280325ffd40692be563393fb32cc4
Adam Robson <adamray312@gmail.com>
2022-08-09 11:07:58 -0700

    'format repo to pass CI'

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const sloganDisplay = document.getElementById('slogan-display');
 const cityImage = document.getElementById('city-image');
 const countryImage = document.getElementById('country-image');
 const cityFeature = document.getElementById('city-feature');
-const waterImage = document.getElementById('water-image')
-const waterFeature = document.getElementById('water-feature')
-const countryFeature = document.getElementById('country-feature')
+const waterImage = document.getElementById('water-image');
+const waterFeature = document.getElementById('water-feature');
+const countryFeature = document.getElementById('country-feature');
 
 const displayStats = document.getElementById('display-stats');
 
-let cityCount= 0;
+let cityCount = 0;
 let waterCount = 0; 
 let countryCount = 0;
 
@@ -22,44 +22,44 @@ let slogansArray = [];
 
 addSloganButton.addEventListener('click', () => {
 
-        sloganDisplay.textContent = '';
-        let userSlogan = sloganInput.value;
-        slogansArray.push(userSlogan);
-        sloganDisplay.textContent = slogansArray;
-        sloganInput.value = '';
-})
+    sloganDisplay.textContent = '';
+    let userSlogan = sloganInput.value;
+    slogansArray.push(userSlogan);
+    sloganDisplay.textContent = slogansArray;
+    sloganInput.value = '';
+});
 
 addCityButton.addEventListener('click', () =>
 {
-        cityNameDisplay.textContent = '';
-        let userCityName = cityNameInput.value;
-        cityNameDisplay.textContent = userCityName;
-        cityNameInput.value = '';
+    cityNameDisplay.textContent = '';
+    let userCityName = cityNameInput.value;
+    cityNameDisplay.textContent = userCityName;
+    cityNameInput.value = '';
 });     
 
 cityFeature.addEventListener('change', () => {
-        cityImage.src = `./assets/${cityFeature.value}.png`
-        cityImage.alt = 'a photo of a city feature'
-        cityCount++;
-        displayStatsFunction();
-})
+    cityImage.src = `./assets/${cityFeature.value}.png`;
+    cityImage.alt = 'a photo of a city feature';
+    cityCount++;
+    displayStatsFunction();
+});
 
 countryFeature.addEventListener('change', () => {
-        countryImage.src = `./assets/${countryFeature.value}-country.png`
-        countryImage.alt = 'a photo of a coutry feature'
-        countryCount++;
-        displayStatsFunction();
-})
+    countryImage.src = `./assets/${countryFeature.value}-country.png`;
+    countryImage.alt = 'a photo of a coutry feature';
+    countryCount++;
+    displayStatsFunction();
+});
 
 waterFeature.addEventListener('change', () => {
-        waterImage.src = '';
-        waterImage.src = `./assets/${waterFeature.value}-water.png`
-        waterImage.alt = 'a photo of a water feature'
-        waterCount++;
-        displayStatsFunction();
-})
+    waterImage.src = '';
+    waterImage.src = `./assets/${waterFeature.value}-water.png`;
+    waterImage.alt = 'a photo of a water feature';
+    waterCount++;
+    displayStatsFunction();
+});
 
 function displayStatsFunction() {
-        displayStats.textContent = '';
-        displayStats.textContent = `You have changed the city feature ${cityCount} times, the country feature ${countryCount} times and the water feature ${waterCount} times.`;
+    displayStats.textContent = '';
+    displayStats.textContent = `You have changed the city feature ${cityCount} times, the country feature ${countryCount} times and the water feature ${waterCount} times.`;
 }
